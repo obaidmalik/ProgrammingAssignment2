75d97f729dd3432a84192060811628e0182f8b2d
Obaid Malik <obaidzpeace@gmail.com>
2021-02-04 00:36:04 +0530

    Matrix inversion logic changes

--- cachematrix.R
+++ cachematrix.R
@@ -26,7 +26,7 @@ makeCacheMatrix <- function(x = matrix()) {
 
 ## Return the inverse matrix by calling the functions defined above
 ## Please ignore this comment - just for demo
-## Please ignore - wip feature change 
+## Please ignore - wip feature change - second change
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
